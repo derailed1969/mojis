7adb1f70c2ec3d915e6f4ca718af545e49a80301
daniel <88695260+derailed1969@users.noreply.github.com>
2022-02-11 03:13:14 +0000

    actually added things

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+import os
+from collections import defaultdict
+
+client = discord.Client()
+forgor = '💀'
+token = os.environ['TOKEN']
+allowedG = {}
+sorry = "sorry i didn't mean to make you mad"
+
+@client.event
+async def on_ready():
+  print("hi")
+
+@client.event
+async def on_message(message):
+  tempID = message.channel.guild.id
+  try:
+    if allowedG[tempID] == 1:
+      if message.content.startswith('shut the fuck up you stupid cunt'):
+        allowedG[tempID] = 0
+        await message.channel.send("sohrry i didn;tt me an to make oyyu ma,;d")
+      if "forgor" in message.content.lower() and not message.author == client.user:
+        await message.add_reaction(forgor)
+      if "forgot" in message.content.lower():
+        await message.channel.send("* forgor")
+    else:
+      if message.content.startswith('i love you mojis'):
+        allowedG[tempID] = 1
+        await message.channel.send("ok")
+  except KeyError:
+    if message.content.startswith('i love you mojis'):
+        allowedG[tempID] = 1
+        await message.channel.send("ok")
+client.run(token)
